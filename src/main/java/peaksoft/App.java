dc6c2159aad9cf0b709d6bef5554dfe015d2816f
ergeshmametuulukanat <ergeshmametuulukanat@gmail.com>
2021-03-30 23:51:19 +0600

    init

---
+++ src/main/java/peaksoft/App.java
@@ -0,0 +1,30 @@
+package peaksoft;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import peaksoft.config.AppConfig;
+import peaksoft.model.AnimalsCage;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        for (int i = 0; i < 5; i++) {
+            AnimalsCage bean =
+                    applicationContext.getBean(AnimalsCage.class);
+            bean.whatAnimalSay();
+        }
+    }
+}
+//    Link to project
+//        Жогорудагы ссылка менен кирип проектти кочурунуз.
+//
+//        1) Dog деген класс тузуп Animal деген класстан наследование кылыныз жана Dog классын компонент кылыныз.
+//        2) Программаны иштетип корунуз. NoUniqueBeanDefinitionException деген исключение тушуш керек.
+//           Эки бин бар Animal деген тип менен ошондуктан ушундай исключение тушуп жатат.
+//        3) AnimalsCageти Dog деген бин менен байланыштырыныз Animal деген класс колдонуп.
+//        4) Timer классынын негизинде бин тузунуз жана AnimalsCage менен байланыштырыныз.
+//        5) main методун иштетип текшериниз, таймер жазган убакыт бир болуш керек.
+//        6) Testти комментарийден чыгарып иштетип корунуз.
+
---
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -0,0 +1,17 @@
+package peaksoft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import peaksoft.model.AnimalsCage;
+import peaksoft.model.Timer;
+
+@Configuration
+@ComponentScan(basePackages = "peaksoft")
+public class AppConfig {
+
+    @Bean
+    public Timer timer(){
+        return new Timer();
+    }
+}
